656d9aa6f0e329b3552ebe36fc04819c7d38117b
fdaumas <florian.daumas.git@gmail.com>
2023-08-13 04:07:16 +0200

    just split main

---
+++ scop/src/main_loop.rs
@@ -0,0 +1,19 @@
+pub fn main_loop(sdl: sdl2::Sdl, window: sdl2::video::Window) -> i8 {
+
+    let mut event_pump = sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit {..} => break 'main,
+                _ => {},
+            }
+        }
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.gl_swap_window();
+    }
+    return 0;
+}
