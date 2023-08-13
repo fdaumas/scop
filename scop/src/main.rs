67d4fa6c4b9ab8972227b576fa7400955e08804d
fdaumas <florian.daumas.git@gmail.com>
2023-08-13 02:59:32 +0200

    make a window

--- scop/src/main.rs
+++ scop/src/main.rs
@@ -1,13 +1,43 @@
-use sdl2::Sdl;
-
 extern crate sdl2;
+extern crate gl;
 
 fn main() {
     let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
 
-    let sdl_context = match sdl
-    {
-        Ok(sdl_context) => sdl_context,
-        Err(error) => panic!("problem init sdl2: {}", error),
-    };
+    // let gl_attr = video_subsystem.gl_attr();
+    //
+    // gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    // gl_attr.set_context_version(4, 5);
+
+    let window = video_subsystem
+        .window("scop", 900, 700)
+        .resizable()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let _gl_context = window.gl_create_context().unwrap();
+    let _gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+
+    unsafe {
+        // gl::Viewport(0, 0, 900, 700);
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+    }
+
+    let mut event_pump = sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit {..} => break 'main,
+                _ => {},
+            }
+        }
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.gl_swap_window();
+    }
 }
